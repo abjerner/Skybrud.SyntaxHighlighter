a439dbbca36bf5e2bfc35c3eda6879db007525b1
Anders Bjerner <abjerner@skybrud.dk>
2016-02-02 23:05:05 +0100

    Added support for extending the default JSON highlighter + XML documentation

--- src/Skybrud.SyntaxHighlighter/Highlighters/Json/JsonHighlighter.cs
+++ src/Skybrud.SyntaxHighlighter/Highlighters/Json/JsonHighlighter.cs
@@ -4,14 +4,24 @@ using System.Web;
 
 namespace Skybrud.SyntaxHighlighter.Highlighters.Json {
 
-    internal class JsonHighlighter {
+    public class JsonHighlighter {
 
-        public string Highlight(string source) {
-            List<object> tokens = JsonTokenizer.GetTokens(source ?? "");
+        /// <summary>
+        /// Highlights the specified JSON string.
+        /// </summary>
+        /// <param name="str">The JSON strin to be highlighted.</param>
+        /// <returns></returns>
+        public virtual string Highlight(string str) {
+            List<object> tokens = JsonTokenizer.GetTokens(str ?? "");
             return "<div class=\"highlight json\"><pre>" + ToHtml(tokens) + "</pre></div>";
         }
 
-        private string ToHtml(List<object> list) {
+        /// <summary>
+        /// Converts a list of tokens into the highlighted HTML.
+        /// </summary>
+        /// <param name="list">The list of tokens.</param>
+        /// <returns>Returns the generated HTML.</returns>
+        protected virtual string ToHtml(List<object> list) {
             string temp = "";
             foreach (object obj in list) {
                 List<object> sublist = obj as List<object>;
@@ -20,23 +30,50 @@ namespace Skybrud.SyntaxHighlighter.Highlighters.Json {
                 } else {
                     string str = obj + "";
                     if (str == "null" || str == "false" || str == "true") {
-                        temp += "<span class=\"constant\">" + str + "</span>";
+                        temp += RenderConstant(str);
                     } else if (Regex.IsMatch(str, "^[0-9]+$")) {
-                        temp += "<span class=\"constant\">" + str + "</span>";
+                        temp += RenderNumber(str);
                     } else if (Regex.IsMatch(str, "^[0-9]+\\.[0-9]+$")) {
-                        temp += "<span class=\"constant\">" + str + "</span>";
+                        temp += RenderNumber(str);
                     } else if (Regex.IsMatch(str, "^-[0-9]+$")) {
-                        temp += "<span class=\"constant\">" + str + "</span>";
+                        temp += RenderNumber(str);
                     } else if (Regex.IsMatch(str, "^-[0-9]+\\.[0-9]+$")) {
-                        temp += "<span class=\"constant\">" + str + "</span>";
+                        temp += RenderNumber(str);
                     } else if (str.StartsWith("\"") && str.EndsWith("\"")) {
-                        temp += "<span class=\"string\">" + HttpUtility.HtmlEncode(str) + "</span>";
+                        temp += RenderString(str);
                     } else {
                         temp += str;
                     }
                 }
             }
             return temp;
+        }
+
+        /// <summary>
+        /// Renders the HTML for a JSON constant.
+        /// </summary>
+        /// <param name="value">The value of the constant.</param>
+        /// <returns>Returns the HTML representing the constant.</returns>
+        protected virtual string RenderConstant(string value) {
+            return "<span class=\"constant\">" + value + "</span>";
+        }
+
+        /// <summary>
+        /// Renders the HTML for a JSON number.
+        /// </summary>
+        /// <param name="value">The number.</param>
+        /// <returns>Returns the HTML representing the number.</returns>
+        protected virtual string RenderNumber(string value) {
+            return "<span class=\"constant\">" + value + "</span>";
+        }
+
+        /// <summary>
+        /// Renders the HTML for a JSON string.
+        /// </summary>
+        /// <param name="value">The value of the string.</param>
+        /// <returns>Returns the HTML representing the string.</returns>
+        protected virtual string RenderString(string value) {
+            return "<span class=\"string\">" + HttpUtility.HtmlEncode(value) + "</span>";
         }
     
     }
