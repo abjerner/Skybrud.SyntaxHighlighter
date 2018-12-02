933fda38d1024f85301df353721c692c7de29823
Anders Bjerner <abjerner@skybrud.dk>
2018-12-02 22:40:46 +0100

    Added new Language enum for representing the supported languages

---
+++ src/Skybrud.SyntaxHighlighter/Language.cs
@@ -0,0 +1,15 @@
+﻿namespace Skybrud.SyntaxHighlighter {
+
+    /// <summary>
+    /// Enum class representing the supported languages of this package.
+    /// </summary>
+    public enum Language {
+        None,
+        CSharp,
+        Json,
+        JavaScript,
+        Html,
+        Xml
+    }
+
+}
