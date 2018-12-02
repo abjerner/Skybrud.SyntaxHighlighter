7d09c09d440699269a2224e102fda6dd08043048
Anders Bjerner <abjerner@skybrud.dk>
2018-12-02 22:49:01 +0100

    Added some exception handling

--- src/Skybrud.SyntaxHighlighter/Highlighter.cs
+++ src/Skybrud.SyntaxHighlighter/Highlighter.cs
@@ -52,11 +52,19 @@ namespace Skybrud.SyntaxHighlighter {
         }
 
         public static string HighlightCSharp(string source) {
-            return new CSharpHighlighter().Highlight(source);
+            try {
+                return new CSharpHighlighter().Highlight(source);
+            } catch (Exception) {
+                return "<div class=\"highlight\"><pre>" + source + "</pre></div>";
+            }
         }
 
         public static string HighlightXml(string source) {
-            return new XmlHighlighter().Highlight(source);
+            try {
+                return new XmlHighlighter().Highlight(source);
+            } catch (Exception) {
+                return "<div class=\"highlight\"><pre>" + source + "</pre></div>";
+            }
         }
 
         public static string HighlightXml(XNode xml) {
@@ -74,26 +82,38 @@ namespace Skybrud.SyntaxHighlighter {
         }
 
         public static string HighlightJavaScript(string source) {
-            return new JavaScriptHighlighter().Highlight(source);
+            try {
+                return new JavaScriptHighlighter().Highlight(source);
+            } catch (Exception) {
+                return "<div class=\"highlight\"><pre>" + source + "</pre></div>";
+            }
         }
 
         public static string HighlightJson(string source) {
-            return new JsonHighlighter().Highlight(source);
+            try {
+                return new JsonHighlighter().Highlight(source);
+            } catch (Exception) {
+                return "<div class=\"highlight\"><pre>" + source + "</pre></div>";
+            }
         }
 
         public static string HighlightJson(string source, Formatting formatting) {
             source = JToken.Parse(source).ToString(formatting);
-            return new JsonHighlighter().Highlight(source);
+            try {
+                return new JsonHighlighter().Highlight(source);
+            } catch (Exception) {
+                return "<div class=\"highlight\"><pre>" + source + "</pre></div>";
+            }
         }
 
         public static string HighlightJson(JToken token) {
             if (token == null) throw new ArgumentNullException(nameof(token));
-            return new JsonHighlighter().Highlight(token + "");
+            return HighlightJson(token.ToString());
         }
 
         public static string HighlightJson(JToken token, Formatting formatting) {
             if (token == null) throw new ArgumentNullException(nameof(token));
-            return new JsonHighlighter().Highlight(token.ToString(formatting));
+            return HighlightJson(token.ToString(formatting));
         }
 
     }
