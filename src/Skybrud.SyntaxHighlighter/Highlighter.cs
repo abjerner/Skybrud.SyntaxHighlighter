2859e9e0455f665fb125bbbc14636b110afbb6cb
Anders Bjerner <abjerner@skybrud.dk>
2018-12-02 22:41:32 +0100

    Implemented new Highlight method

--- src/Skybrud.SyntaxHighlighter/Highlighter.cs
+++ src/Skybrud.SyntaxHighlighter/Highlighter.cs
@@ -10,6 +10,46 @@ using Skybrud.SyntaxHighlighter.Highlighters.Xml;
 namespace Skybrud.SyntaxHighlighter {
 
     public class Highlighter {
+        
+        /// <summary>
+        /// Highlights the specified <paramref name="source"/>.
+        /// </summary>
+        /// <param name="source">The source / code to be highlighted.</param>
+        /// <param name="language">The language to be used.</param>
+        /// <returns>The highlighted source.</returns>
+        public static string Highlight(string source, Language language) {
+
+            try {
+
+                switch (language) {
+
+                    case Language.CSharp:
+                        return HighlightCSharp(source);
+
+                    case Language.Json:
+                        return HighlightJson(source);
+
+                    case Language.JavaScript:
+                        return HighlightJavaScript(source);
+
+                    case Language.Html:
+                        return HighlightHtml(source);
+
+                    case Language.Xml:
+                        return HighlightXml(source);
+
+                    default:
+                        return "<div class=\"highlight\"><pre>" + source + "</pre></div>";
+
+                }
+
+            } catch (Exception) {
+
+                return "<div class=\"highlight\"><pre>" + source + "</pre></div>";
+
+            }
+
+        }
 
         public static string HighlightCSharp(string source) {
             return new CSharpHighlighter().Highlight(source);
